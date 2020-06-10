387b6578f9de59fe62aad7c4c1a779d2e9dfbf6e
Léa Michel <leamichel1497@gmail.com>
2020-06-10 13:02:45 +0200

    Valid Model

--- indirect_operations_costs.py
+++ indirect_operations_costs.py
@@ -9,18 +9,19 @@ class IndirectOperationsCosts(IndirectOperationsCostsBase):
     """ An OpenMDAO component to encapsulate IndirectOperationsCosts discipline """
 		
     def compute(self, inputs, outputs):
-        """ IndirectOperationsCosts computation """
-        if self._impl:
-            # Docking mechanism: use implementation if referenced in .whatsopt_dock.yml file
-            self._impl.compute(inputs, outputs)
-         else:
-            LpA = inputs['LpA']
-            S = inputs['S']
-            W = inputs['W']
-
-            C_IO = (40 * S + 24) * LpA**-0.379 * W /1000
-                    
-            outputs['C_IO'] = C_IO
+      """ IndirectOperationsCosts computation """
+      if self._impl:
+        # Docking mechanism: use implementation if referenced in .whatsopt_dock.yml file
+        self._impl.compute(inputs, outputs)
+      else:
+        LpA = inputs['LpA']
+        S = inputs['S']
+        W = inputs['W']
+        
+        C_IO = (40 * S + 24) * LpA**-0.379 * W /1000
+        
+        outputs['C_IO'] = C_IO
+        
         return outputs 
 # Reminder: inputs of compute()
 #   
